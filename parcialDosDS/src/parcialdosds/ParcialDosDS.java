310179182a01251ed5e09629ab240b10831aa1fe
Valeria <05valarist@gmail.com>
2023-10-03 10:30:13 -0500

    inicio

---
+++ parcialDosDS/src/parcialdosds/ParcialDosDS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parcialdosds;
+
+/**
+ *
+ * @author val
+ */
+public class ParcialDosDS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
